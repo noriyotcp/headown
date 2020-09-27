5160fca775dd15ac317119a22c621e1597497e27
noriyotcp <noriyo.akita@gmail.com>
2020-09-27 20:58:35 +0900

    Extract from sample markdown file

--- lib/headown/cli.rb
+++ lib/headown/cli.rb
@@ -1,10 +1,43 @@
 require 'thor'
+require 'commonmarker'
+require 'open-uri'
 
 module Headown
   class CLI < Thor
     desc 'extract <path>', 'extract headers from file path'
+
     def extract(file_path)
-      ['# h1', '## h2']
+      file_data = URI.open(file_path).read
+      extract_headers(file_data)
+    end
+
+    private
+
+    def header_nodes(data)
+      doc = CommonMarker.render_doc(data)
+      [].tap do |nodes|
+        doc.walk do |node|
+          if node.type == :header
+            nodes << node
+          end
+        end
+      end
+    end
+
+    def build_header(node)
+      text = ''
+      node.each do |subnode|
+        if subnode.type == :text
+          text = subnode.string_content
+        end
+      end
+      '#' * node.header_level + " #{text}"
+    end
+
+    def extract_headers(file_data)
+      header_nodes(file_data).map do |node|
+        build_header(node)
+      end
     end
   end
 end
--- spec/headown/cli_spec.rb
+++ spec/headown/cli_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Headown::CLI do
   it 'extract headers' do
-    expect(described_class.new.extract('./sample.md')).to eq ['# h1', '## h2']
+    expect(described_class.new.extract('spec/headown/sample.md')).to eq ["# h1", "## h2", "### h3", "#### h4", "##### h5", "###### h6"]
   end
 end
